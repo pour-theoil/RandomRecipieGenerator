a971e741d4835c2101bee6282c9e336817468084
Logan Demmy <frank.demmy@gmail.com>
2021-08-22 13:14:30 -0500

    recipe displays with image, type and summary

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { getRandomRecipe} from './Modules/ApiManager';
 
 function App() {
   const [recipe, setRecipe] = useState({})
-
+  
   const getRecipe = () => {
     getRandomRecipe().then(response => setRecipe(response.recipes[0]))
   }
@@ -16,7 +16,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Random Recipe Generator</h1>
+        {/* <button onclick={}></button> */}
         <h2>{recipe.title}</h2>
+        <img src={recipe.image} alt="Dish" />
+        <p>Dish Type: {recipe.dishTypes}</p>
+        <p>Summary: {recipe.summary}</p>
       </header>
     </div>
   );
